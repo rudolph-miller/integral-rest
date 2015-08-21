71c1e92f2efd9b9574724fd0f3d53dc5824fb959
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-08-21 02:19:35 +0000

    update docs at 1440123575. [ci skip]

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:integral-rest)
+ :documents ((:title "INTEGRAL-REST"
+              :authors ("Rudolph-Miller")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
